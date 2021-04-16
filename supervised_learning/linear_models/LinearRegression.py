71a704df50093557df6f54f5942103fabd466ab8
Mitchell Mirano <mitchellmirano25@gmail.com>
2021-04-16 00:48:40 -0500

    we create file LinearRegresion.py

---
+++ supervised_learning/linear_models/LinearRegression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+x=np.linspace(1,10,100)
+y= 2*x +2 + 3 + np.random.randn(100)
+
+
+dic={'x':x,'y':y}
+df=pd.DataFrame(dic)
+
+
+plt.plot(x,y)
