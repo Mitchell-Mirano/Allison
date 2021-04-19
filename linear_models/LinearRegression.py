1b00b3982105c3e6f187070daafcc24c93026ceb
Mitchell Mirano <mitchellmirano25@gmail.com>
2021-04-19 15:32:06 -0500

    first version of Linear Regresion completed

--- linear_models/LinearRegression.py
+++ linear_models/LinearRegression.py
@@ -1,14 +1,46 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
+class LinearRegresion:
+    
+  def __init__(self,features,labels):
+      self.features=np.append([np.ones(features.to_numpy().T.shape[1])], features.to_numpy().T, axis=0)
+      self.weights=np.random.randn(len(self.features))
+      self.labels=labels.to_numpy().T
 
-x=np.linspace(1,10,100)
-y= 2*x +2 + 3 + np.random.randn(100)
+  def foward(self):
+    Linear_convination=np.array([self.weights[i]*self.features[i] for i in range(len(self.weights))])
+    predictions=np.sum(Linear_convination,axis=0)
+    return predictions
 
+  def decm_dw(self, predictions):
+    gradients=np.zeros(len(self.weights))
+    for  i in range(len(self.weights)):
+      gradients[i]=(-2/self.labels.shape[1])*np.dot(self.labels-predictions,self.features[i])
+    return gradients
+  
+  def bacward(self,gradients,lr):
+    new_weigths=self.weights-lr*gradients
+    self.weights=new_weigths
 
-dic={'x':x,'y':y}
-df=pd.DataFrame(dic)
+  def train(self,lr,n_iters):
+    for i in range(n_iters):
+      predictions=self.foward()
+      gradients=self.decm_dw(predictions)
+      self.bacward(gradients,lr)
 
-plt.Figure(figsize=(20,8))
-plt.plot(x,y)
+  def r2_score(self):
+    errors=np.sum((self.labels-self.foward())**2)
+    varianza=self.labels.var()*self.labels.shape[1]
+    return 1-(errors/varianza)
+
+  def predict (self,features):
+    data=np.append([np.ones(features.to_numpy().T.shape[1])], features.to_numpy().T, axis=0)
+    Linear_convination=np.array([self.weights[i]*data[i] for i in range(len(self.weights))])
+    predictions=np.sum(Linear_convination,axis=0)
+    return predictions
+  
+  def save_weights(self,path):
+    weights=pd.DataFrame(self.weights)
+    weights.to_csv(path,index=False)
+    
