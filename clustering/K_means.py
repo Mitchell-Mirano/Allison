2a090bf71814af75c9aa7e1957b1339dbef483cd
Mitchell Mirano <mitchellmirano25@gmail.com>
2021-04-24 22:10:03 -0500

    update the Kmeans class

--- clustering/K_means.py
+++ clustering/K_means.py
@@ -1,60 +1,77 @@
 # -*- coding: utf-8 -*-
-import numpy as np 
-
+import numpy as np
 class Kmeans:
+
+  def __init__(self):
+    self.centroids=None
+    self.history_train={}
+    self.labels=None
+
+  def data_preprocessing_train(self,features,n_centroids, history_train):
+
+    features_train=features.to_numpy()
     
-    def __init__(self,features,n_centroides):
-        self.features=features.to_numpy()
-        self.n_centroides=n_centroides
-        self.centroides_init=self.features[np.random.choice(len(self.features),self.n_centroides)]
-        self.labels=np.zeros(len(self.features))
-        self.centroides=np.empty([self.n_centroides,self.features.shape[1]])
+    self.centroids=features_train[np.random.randint(0, len(features_train),n_centroids )]
 
-    def distances(self,features, centroides):
-      distances_features_to_centroides=[]
-      for feature in features:
-        distances=[]
-        for centroide in centroides:
-          distances.append(np.linalg.norm(feature-centroide))
-        distances_features_to_centroides.append(distances)
-      return np.array(distances_features_to_centroides)
+    if history_train==True:
+      self.history_train[0]=self.centroids
 
-    def update_values(self,new_array,old_array):
-      for i in range(len(new_array)):
-        old_array[i]=new_array[i]
+    return features_train
 
-    def new_centroides(self,labels,features):
-      new_centroides=[]
-      for label in set(labels):
-        filter=np.where(labels==label)
-        vectors=features[filter]
-        media_vectors=vectors.sum(axis=0)/len(vectors)
-        new_centroides.append(media_vectors)
 
-      return new_centroides
+  def distances(self,features, centroids):
+    distances_features_centroides=[]
+    for feature in features:
+      distances_feature=[]
+      for centroid in centroids:
+        distances_feature.append(np.linalg.norm(centroid-feature))
 
-    def train(self, error=0.01):
-      movement_init=10000
-      iter=0
-      while movement_init>error:
-        iter=iter+1
-        if iter==1:
-          distances=self.distances(self.features,self.centroides_init)
-        else :
-          distances=self.distances(self.features,self.centroides)
+      distances_features_centroides.append(distances_feature)
 
-        new_labels=np.array([np.where(distance==np.min(distance))[0][0] for distance in distances])
-        self.update_values(new_labels,self.labels)
-        new_centroides=self.new_centroides(self.labels,self.features)
+    return np.array(distances_features_centroides)
 
-        if iter==1:
-          movement=np.mean(np.array([np.linalg.norm(centroide-new_centroide) for centroide, new_centroide in zip(self.centroides_init,np.array(new_centroides))]))
-        else:
-          movement=np.mean(np.array([np.linalg.norm(centroide-new_centroide) for centroide, new_centroide in zip(self.centroides,np.array(new_centroides))]))
-        
-        self.update_values(new_centroides,self.centroides)
+  def new_labels(self,distances):
+    new_labels=np.array([np.where(distance==np.min(distance))[0][0] for distance in distances])
+    return new_labels
 
-        movement_init=movement
+  def new_centroids(self, features, labels):
+    new_centroids=[]
+    for label in set(labels):
+      filter=np.where(labels==label)
+      vectors=features[filter]
+      media_vectors=vectors.sum(axis=0)/len(vectors)
+      new_centroids.append(media_vectors)
 
+    return np.array(new_centroids)
+  
+  def moviment(self,centroids_beafore, centroids_after):
+    moviments=[]
+    for centroid_bf, centroid_af in zip(centroids_beafore,centroids_after):
+      moviments.append(np.linalg.norm(centroid_bf-centroid_af))
+
+    return np.mean(moviments)
+
+
+
+  def train(self,features,n_centroids,moviment_limit=0.1, history_train=True):
+
+    features_train=self.data_preprocessing_train(features,n_centroids, history_train)
+
+    iter=0
+    while True:
+      iter=iter+1
+      distances=self.distances(features_train,self.centroids)
+      self.labels=self.new_labels(distances)
+
+      centroids_before=self.centroids
+      self.centroids=self.new_centroids(features_train, self.labels)
+
+      if history_train==True:
+        self.history_train[iter]=self.centroids
+
+      moviment=self.moviment(centroids_before,self.centroids)
+      print('Iter: {} \t {} \t moviment: {:.2f}'.format(iter,50*'='+'>',moviment))
+      if moviment< moviment_limit:
+        break
 
  
