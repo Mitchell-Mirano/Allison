ee4783d54ceda18edb9d83b188cd4bb46d2dc393
Mitchell Mirano <mitchellmirano25@gmail.com>
2021-05-09 03:30:15 -0500

    LinearRegression updated

--- linear_models/_linear_regression.py
+++ linear_models/_linear_regression.py
@@ -2,90 +2,90 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class LinearRegression:
-  
-  def __init__(self,type_regression='simple',functions=['lineal'],regularization=None):
-    self.type_regression=type_regression
-    self.functions=functions
-    self.regularization=regularization
+class LinearRegression():
+
+  def __init__(self, regularization=None):
     self.weights=None
+    self.bias=np.random.rand()
+    self.regularization=None
+    self.loss_function=None
+    self.metrics=None
+    self.learning_ratio=None
 
-  def data_preproscessing_train(self, features, labels):
-    if self.type_regression=='simple':
-      self.weights=np.random.randn(2)
-      features_train=features.to_numpy()
+  def optimizers(self, loss_function,lr,metrics,regularization=None):
+    self.loss_function=loss_function
+    self.learning_ratio=lr
+    self.metrics=metrics
+    self.regularization=regularization
 
-    if self.type_regression=='multiple':
-      features_train=features.to_numpy()
-      self.weights=np.random.randn(len(features_train[0])+1)
-
-    return features_train, labels.to_numpy()
-
-  def data_preproscessing_predict(self, features):
-    features_train=features.to_numpy()
-    return features_train
-
-
-  def foward(self, features):
-    if self.type_regression=='simple':
-      predictions=self.weights[0] + features*self.weights[1] 
-
-    if self.type_regression=='multiple':
-      predictions=self.weights[0] + np.sum(features*self.weights[1:], axis=1)
-
-    return predictions
+    
+  def init_params(self,features):
+    if features.ndim==1:
+      self.weights=np.random.rand(1)
+    else :
+      self.weights=np.random.rand(features.shape[0])
 
 
-  def decm_dw(self, predictions, labels, features):
-    n=len(labels)
-    gradients=np.zeros(len(self.weights))
-    if self.type_regression=='simple':
-      gradients[0]=(-2/n)*np.sum(labels-predictions)
-      gradients[1]=(-2/n)*np.dot(labels-predictions,features)
+  def foward(self,features):
+    if len(self.weights)==1:
+      prediction= self.bias + self.weights*features
+    else:
+      prediction=self.bias + self.weights@features
 
-    if self.type_regression=='multiple':
-      gradients[0]=(-2/n)*np.sum(labels-predictions)
-      for  i in range(1,len(self.weights)-1):
-        gradients[i]=(-2/n)*np.dot(labels-predictions,features.T[i])
-  
-    return gradients
+    return prediction
 
 
-  def bacward(self,gradients,lr):
-    self.weights=self.weights-lr*gradients
-  
+  def  bacward(self,labels,predictions, features):
 
-  def train(self,lr,n_iters, features, labels):
-    features_train, labels_train=self.data_preproscessing_train(features, labels)
+    gradient=self.loss_function(labels, predictions,derivative=True)
+
+    if features.ndim==1:
+      gradient_weights=gradient*np.mean(features)
+    else:
+      gradient_weights=gradient*np.mean(features, axis=1)
+
+    self.bias =self.bias-self.learning_ratio*gradient
+    self.weights= self.weights-self.learning_ratio*gradient_weights
+
+
+
+  def train(self,n_iters,features, labels, callbacks_period=2):
+    self.init_params(features)
+
+    history_train={
+        'iter':[],
+        'loss':[],
+        'r2_score':[]
+    }
+
     for i in range(n_iters):
-      predictions=self.foward(features_train)
-      gradients=self.decm_dw(predictions,labels_train, features_train)
-      self.bacward(gradients,lr)
-      score=self.r2_score(labels_train,predictions)
-      if (i+1)%2==0:
-        print('Iter:\t{}\t{}\tr2_score:\t{:.2f}%'.format(i+1,50*'='+'>',score))
+      predictions=self.foward(features)
+      self.bacward(labels, predictions, features)
 
-    if self.type_regression=='simple':
-      plt.figure(figsize=(12,6))
-      plt.scatter(features,labels)
-      plt.plot(features,predictions,c='r', lw=3)
-      plt.show()
+      if (i+1)%callbacks_period==0:
+        score=self.metrics(labels,predictions)
+        loss=self.loss_function(labels, predictions)
+        history_train['loss'].append(loss)
+        history_train['r2_score'].append(score)
+        history_train['iter'].append(i+1)
+        print('Iter:\t{}\t{}\t r2_score:\t{:.2f}%'.format(i+1,50*'='+'>',score))
 
-  def r2_score(self, labels, predictions):
-    errors=np.sum((labels-predictions)**2)
-    varianza=labels.var()*len(labels)
-    return (1-(errors/varianza))*100
+    return history_train
+
 
   def predict (self,features):
-    data_predict=self.data_preproscessing_predict(features)
-    labels_predict=self.foward(data_predict)
-    return labels_predict
+    predictions=self.foward(features)
+    return predictions
   
   def save_weights(self,path):
-    weights=pd.DataFrame(self.weights)
-    weights.to_csv(path,index=False)
+    with open(path, 'wb') as f:
+      np.save(f,self.bias)
+      np.save(f,self.weights)
+
   
   def load_weights(self,path):
-    data=pd.read_csv(path)
-    weights_charge=data['0']
-    self.weights=weights_charge.to_numpy()
+    with open(path, 'rb') as f:
+      bias = np.load(f)
+      weights = np.load(f)
+    self.bias= bias
+    self.weights=weights
