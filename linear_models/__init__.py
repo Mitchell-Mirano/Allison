762d7a6a3bdadde27ee56a6f81e3eac2739b33a1
Mitchell Mirano <mitchellmirano25@gmail.com>
2021-05-16 00:43:53 -0500

    PolinomicRegression model aggregated

--- linear_models/__init__.py
+++ linear_models/__init__.py
@@ -2,7 +2,8 @@
 
 from ._linear_regression import LinearRegression
 from ._logistic_regression import LogisticRegression
-
+from ._polinomic_regression import PolinomicRegression
 
 __all__=['LinearRegression',
-        'LogisticRegression']
+        'LogisticRegression',
+        'PolinomicRegression']
